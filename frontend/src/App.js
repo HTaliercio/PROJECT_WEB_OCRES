b3aa9d198a726d8f1ef6b62f710cb1109e5360e7
HTaliercio <63606066+HTaliercio@users.noreply.github.com>
2020-11-30 02:41:37 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap-4-grid/css/grid.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ReactDOM from 'react-dom';
+import { Button } from '@progress/kendo-react-buttons';
+import { savePDF } from '@progress/kendo-react-pdf';
+import { Ripple } from '@progress/kendo-react-ripple';
+import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
+import { Input } from '@progress/kendo-react-inputs'; 
+import '@progress/kendo-theme-material/dist/all.css';
+import { Graph1 } from './composants/Graph1';
+import { Graph2 } from './composants/Graph2';
+import { TableauGrille } from './composants/Grille1';
+/*import admin from './admin';
+import {Link } from "react-router-dom"; */
+
+
+
+
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.appContainer = React.createRef();
+    this.state = {
+      showDialog: false
+    }
+  }
+
+  handlePDFExport = () => {
+    savePDF(ReactDOM.findDOMNode(this.appContainer), { paperSize: 'auto' });
+  }
+  handleShare = () => {
+    this.setState({
+      showDialog: !this.state.showDialog
+    }, () => console.log(this.state))
+  }
+  render() {
+    return (
+
+      <Ripple>
+      <div className="bootstrap-wrapper">
+        <div className="app-container container"ref={(el) => this.appContainer = el}>
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+              <h1>Ventes | Post-confinement 2020</h1>
+            </div>
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 buttons-right">
+<Button primary={true} onClick={this.handleShare}>Partager</Button>
+      <Button onClick={this.handlePDFExport}>Exporter en PDF</Button>
+</div>
+          </div>
+
+          <div className="row">
+
+            <div className="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+            <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <TableauGrille />
+                </div>
+              </div>
+              <div className="row">
+
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+
+                <Graph2 />
+
+                </div>
+
+
+                <div className="col-xs-6 col-sm-6 col-md-2 col-lg-2 col-xl-2">
+                  <div className="percentage-container">
+                    <span className="percentage-number">94</span>
+                    <span className="percentage-sign">%</span>
+                    <p>POURCENTAGE DE GALERE SUR LE PROJET</p>
+                  </div>
+                  <div className="percentage-container">
+                    <span className="percentage-number">89</span>
+                    <span className="percentage-sign">%</span>
+                    <p>ESTIMATION DE NOTE</p>
+                  </div>
+                </div>
+                <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                <Graph1 />
+                </div>
+              </div>
+
+            </div>
+          </div>
+{this.state.showDialog &&
+  <Dialog title={"Share this report"} onClose={this.handleShare}>
+    <p>Entrez une adresse email</p>
+    <Input placeholder="exemple@projetOCRES.com" />
+    <DialogActionsBar>
+      <Button primary={true} onClick={this.handleShare}>Partager</Button>
+      <Button onClick={this.handleShare}>Quiter</Button>
+    </DialogActionsBar>
+  </Dialog>
+}
+        </div>
+
+      </div>
+      </Ripple>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/data/appData.js
@@ -0,0 +1,127 @@
+export const donutChartData = [
+    { 'foodType': 'Aliments', 'percentSold': 16.5, 'color': "#EADCFC" },
+    { 'foodType': 'Décoration', 'percentSold': 24, 'color': "#DAC0F9" },
+    { 'foodType': 'cours web design', 'percentSold': 13, 'color': "#C08AE6" },
+    { 'foodType': 'Maquillage', 'percentSold': 16.5, 'color': "#D364FA" },
+    { 'foodType': 'Seafood', 'percentSold': 20, 'color': "#B734E6" },
+    { 'foodType': 'Autre', 'percentSold': 10, 'color': "#8D18AD" }
+  ];
+
+
+export const barChartMonthlyPercentages = [
+  { name: 'Aliments', data: [14, 16, 19.5], 'color': "green" },
+  { name: 'Décoration', data: [24, 23.5, 24.5], 'color': "#DAC0F9" },
+  { name: 'Supports de cours de web design', data: [12.5, 12.5, 14], 'color': "#C08AE6" },
+  { name: 'Maquillage', data: [16, 18, 17], 'color': "#D364FA" },
+  { name: 'Seafood', data: [21.5, 20, 17], 'color': "#B734E6" },
+  { name: 'Autre', data: [7, 12, 11], 'color': "#8D18AD" },
+];
+export const barChartQ4Months =['Octobre', 'Novembre', 'Decembre'];
+export const gridData = [
+  {
+      "ProductID": 1,
+      "ProductName": "Boules de Noël",
+      "SupplierID": 1,
+      "CategoryID": 1,
+      "UnitPrice": 18,
+      "UnitsInStock": 39,
+      "UnitsOnOrder": 0,
+      "ReorderLevel": 10,
+      "Discontinued": false,
+      "Category": {
+          "CategoryID": 1,
+          "CategoryName": "Beverages",
+          "Description": "Soft drinks, coffees, teas, beers, and ales"
+      },
+      "FirstOrderedOn": new Date(1996, 8, 20)
+  },
+  {
+      "ProductID": 2,
+      "ProductName": "Guirlandes",
+      "SupplierID": 1,
+      "CategoryID": 1,
+      "UnitPrice": 19,
+      "UnitsInStock": 17,
+      "UnitsOnOrder": 40,
+      "ReorderLevel": 25,
+      "Discontinued": false,
+      "Category": {
+          "CategoryID": 1,
+          "CategoryName": "Beverages",
+          "Description": "Soft drinks, coffees, teas, beers, and ales"
+      },
+      "FirstOrderedOn": new Date(1996, 7, 12)
+  },
+  {
+      "ProductID": 3,
+      "ProductName": "Bob de Noël",
+      "SupplierID": 1,
+      "CategoryID": 2,
+
+      "UnitPrice": 10,
+      "UnitsInStock": 13,
+      "UnitsOnOrder": 70,
+      "ReorderLevel": 25,
+      "Discontinued": false,
+      "Category": {
+          "CategoryID": 2,
+          "CategoryName": "Condiments",
+          "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+      },
+      "FirstOrderedOn": new Date(1996, 8, 26)
+  },
+{
+      "ProductID": 4,
+      "ProductName": "Chaussettes",
+      "SupplierID": 2,
+      "CategoryID": 2,
+
+      "UnitPrice": 22,
+      "UnitsInStock": 53,
+      "UnitsOnOrder": 0,
+      "ReorderLevel": 0,
+      "Discontinued": false,
+      "Category": {
+          "CategoryID": 2,
+          "CategoryName": "Condiments",
+          "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+      },
+      "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+  {
+      "ProductID": 5,
+      "ProductName": "Dinde",
+      "SupplierID": 2,
+      "CategoryID": 2,
+      "UnitPrice": 21.35,
+      "UnitsInStock": 0,
+      "UnitsOnOrder": 0,
+      "ReorderLevel": 0,
+      "Discontinued": true,
+      "Category": {
+          "CategoryID": 2,
+          "CategoryName": "Condiments",
+          "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+      },
+      "FirstOrderedOn": new Date(1996, 7, 17)
+  },
+  {
+      "ProductID": 6,
+      "ProductName": "Marrons",
+      "SupplierID": 3,
+      "CategoryID": 2,
+
+      "UnitPrice": 25,
+      "UnitsInStock": 120,
+      "UnitsOnOrder": 0,
+      "ReorderLevel": 25,
+      "Discontinued": false,
+      "Category": {
+          "CategoryID": 2,
+          "CategoryName": "Condiments",
+          "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+      },
+      "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+];
+
